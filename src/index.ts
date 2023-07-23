410222f717159ef2e06dc9b06d6cddeb9ced1090
Denis Santiago <41819353+denissanthiago@users.noreply.github.com>
2023-07-23 11:04:52 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import router from './router'
+
+const app = express()
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('server on port 8080 🔥');
+})
+
+const MONGO_URL = 'mongodb+srv://denissanthiago:myggoj-7jEnxa-gegwec@cluster0.9okaj71.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router())
